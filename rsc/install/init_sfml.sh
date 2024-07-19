84970ca8edb6ef7aadb5da40cf2af2c886e3c152
Ycaro02 <Ycaro02@hotmail.fr>
2024-07-19 13:18:27 +0200

    Add SDL2 loading

--- rsc/install/init_sfml.sh
+++ rsc/install/init_sfml.sh
@@ -16,24 +16,25 @@ FD_OUT="/dev/stdout"
 # Update FD_OUT if -q option is passed
 handle_quiet_opt "${@}"
 
+function load_deps_SFML {
+	# Create directories
+	display_color_msg ${YELLOW} "Create directories ${DEPS_DIR} and ${INSTALL_DIR}."
+	mkdir -p ${DEPS_DIR}
+	mkdir -p ${INSTALL_DIR}
 
-# Create directories
-display_color_msg ${YELLOW} "Create directories ${DEPS_DIR} and ${INSTALL_DIR}."
-mkdir -p ${DEPS_DIR}
-mkdir -p ${INSTALL_DIR}
+	# set -e  # Exit immediately if a command exits with a non-zero status.
 
-# set -e  # Exit immediately if a command exits with a non-zero status.
+	# Need libudev and libxrandr but they are already installed in the system (TOCHECK on 42 compute)
+	# libudev should be in systemd package, libxrandr (should be aldready installed for running minilibx)
 
-# Need libudev and libxrandr but they are already installed in the system (TOCHECK on 42 compute)
-# libudev should be in systemd package
-# libxrandr (should be aldready installed for running minilibx)
-# Download and install dependencies
-display_double_color_msg ${BLUE} "Download and install dependencies for " ${RED} "SFML"
-load_lib "https://downloads.xiph.org/releases/ogg/libogg-1.3.5.tar.gz"
-load_lib "https://downloads.xiph.org/releases/vorbis/libvorbis-1.3.7.tar.gz" "--with-ogg=${INSTALL_DIR}"
-load_lib "https://downloads.xiph.org/releases/flac/flac-1.3.3.tar.xz" "--disable-cpplibs"
-load_lib "https://sourceforge.net/projects/freetype/files/freetype2/2.11.0/freetype-2.11.0.tar.gz/download"
-load_lib_cmake "https://github.com/kcat/openal-soft/archive/refs/tags/1.23.1.tar.gz" "openal-soft"
+	# Download and install dependencies
+	display_double_color_msg ${BLUE} "Download and install dependencies for " ${RED} "SFML"
+	load_lib "https://downloads.xiph.org/releases/ogg/libogg-1.3.5.tar.gz"
+	load_lib "https://downloads.xiph.org/releases/vorbis/libvorbis-1.3.7.tar.gz" "--with-ogg=${INSTALL_DIR}"
+	load_lib "https://downloads.xiph.org/releases/flac/flac-1.3.3.tar.xz" "--disable-cpplibs"
+	load_lib "https://sourceforge.net/projects/freetype/files/freetype2/2.11.0/freetype-2.11.0.tar.gz/download"
+	load_lib_cmake "https://github.com/kcat/openal-soft/archive/refs/tags/1.23.1.tar.gz" "openal-soft"
+}
 
 function load_SFML {
 	SFML_REPO="${1}"
@@ -52,7 +53,6 @@ function load_SFML {
 	cd ${BUILD_DIR}
 
 	# Configure CMake with local dependencies
-	# echo "Configure CMake variable ..."
 	cmake .. -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
 		-DCMAKE_PREFIX_PATH=${INSTALL_DIR} \
 		-DFLAC_INCLUDE_DIR=${INSTALL_DIR}/include \
@@ -83,8 +83,27 @@ function load_SFML {
 	display_color_msg ${GREEN} "SFML instalation done in ${INSTALL_DIR}."
 }
 
-load_SFML "https://github.com/SFML/SFML.git" "2.6.1"
 
+function load_SDL2 {
+	cd ${BASE_DIR}
+
+	display_color_msg ${YELLOW} "Download and install SDL2..."
+	# Download and install SDL2
+	wget https://github.com/libsdl-org/SDL/releases/download/release-2.30.5/SDL2-2.30.5.tar.gz
+	tar -xvf SDL2-2.30.5.tar.gz
+	rm -rf SDL2-2.30.5.tar.gz
+	cd SDL2-2.30.5
+	mkdir build
+	cd build
+	cmake .. -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_PREFIX_PATH=${INSTALL_DIR}
+	make -s -j$(nproc)
+	make -s install
+	display_color_msg ${GREEN} "SDL2 instalation done in ${INSTALL_DIR}."
+}
+
+load_deps_SFML
+load_SFML "https://github.com/SFML/SFML.git" "2.6.1"
+load_SDL2 >> $FD_OUT 2>&1
 # Old code
 # echo "Dowlnoading libsndfile..."
 # install_library "https://github.com/libsndfile/libsndfile/releases/download/1.2.2/libsndfile-1.2.2.tar.xz" "libsndfile" "tar.xz" >> $FD_OUT 2>&1
