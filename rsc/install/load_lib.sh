b76fafe0c7cede8852b6c460927ad87c24b4c0d4
Ycaro02 <Ycaro02@hotmail.fr>
2024-07-26 11:31:26 +0200

    Try complete path for xcursor

--- rsc/install/load_lib.sh
+++ rsc/install/load_lib.sh
@@ -81,7 +81,7 @@ function load_SFML {
 			-DX11_Xext_LIB=${INSTALL_DIR}/lib/libXext.so \
 			-DX11_X11_INCLUDE_PATH=${INSTALL_DIR}/include/ \
 			-DX11_Xext_INCLUDE_PATH=${INSTALL_DIR}/include/ \
-			-DX11_Xcursor_INCLUDE_PATH=${INSTALL_DIR}/include/ \
+			-DX11_Xcursor_INCLUDE_PATH=${INSTALL_DIR}/include/X11/Xcursor \
 			-DBUILD_SHARED_LIBS=ON \
          	>> $FD_OUT 2>&1
 
@@ -187,6 +187,9 @@ function load_raylib {
 			-DX11_X11_INCLUDE_PATH=${INSTALL_DIR}/include/ \
 			-DX11_Xext_INCLUDE_PATH=${INSTALL_DIR}/include/ \
 			-DX11_Xcursor_INCLUDE_PATH=${INSTALL_DIR}/include/ \
+			-DX11_Xinput_INCLUDE_PATH=${INSTALL_DIR}/include/ \
+			-DX11_Xrandr_INCLUDE_PATH=${INSTALL_DIR}/include/ \
+			-DX11_Xinerama_INCLUDE_PATH=${INSTALL_DIR}/include/ \
 			-DBUILD_SHARED_LIBS=ON \
 			-DBUILD_GLFW=OFF \
 
